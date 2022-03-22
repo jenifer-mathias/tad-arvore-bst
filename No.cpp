648b413cc7a5e7f232988fc5a669af053ab143aa
jenifer-mathias <jenifer.mathias.santos@hotmail.com>
2022-03-21 23:01:25 -0300

    Add header with name of group members
    Signed-off-by: jenifer-mathias <jenifer.mathias.santos@hotmail.com>

--- No.cpp
+++ No.cpp
@@ -1,3 +1,22 @@
+/** ******************************************************************
+ TAD Árvore BST
+ Estrutura de Dados II
+
+ GRUPO
+
+ Nome: Gabriela Lopes Francisco
+ TIA: 31744591
+
+ Nome: Giulia Barbieri Quagliano
+ TIA: 42013070
+
+ Nome: Jenifer Mathias dos Santos
+ TIA: 32092946
+
+ Nome: Lucas Martins de Souza
+ TIA: 31943187
+****************************************************************** */
+
 using namespace std;
 #include "No.h"
 
